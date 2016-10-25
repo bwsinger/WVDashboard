a9e2b33eec06e9f900fdc65c24f172623c881d41
Matthew Favetti <mfavetti@gmail.com>
2016-10-25 11:28:35 -0700

    Rough layouts, hooking up dashboard api

--- web/public/modules/dashboard/controllers/dashboard.client.controller.js
+++ web/public/modules/dashboard/controllers/dashboard.client.controller.js
@@ -5,13 +5,56 @@
 		.module('dashboard')
 		.controller('Dashboard', DashboardController);
 
-	DashboardController.$inject = ['$routeParams'];
+	DashboardController.$inject = ['$routeParams', 'Hobo'];
 
-	function DashboardController($routeParams) {
+	function DashboardController($routeParams, Hobo) {
 
 		var vm = this;
 
+		vm.timespans = ['hourly', 'daily', 'weekly', 'monthly'];
+
 		vm.building = $routeParams.building;
+		vm.changePercentTimespan = changePercentTimespan;
+		vm.changeHistoricalTimespan = changeHistoricalTimespan;
+
+		activate();
+
+		//////////////////////////
+
+		function activate() {
+
+			Hobo.getLeaderboard().then(function(data) {
+				vm.leaderboardData = data;
+			});
+
+			Hobo.getCurrent(vm.building).then(function(data) {
+				vm.currentData = data;
+			});
+
+			//defaults
+			changeHistoricalTimespan(vm.timespans[2]);
+			changePercentTimespan(vm.timespans[2]);
+		}
+
+		function changeHistoricalTimespan(timespan) {
+			if(timespan !== vm.historicalTimespan) {
+				vm.historicalTimespan = timespan;
+
+				Hobo.getHistorical(vm.historicalTimespan, vm.building).then(function(data) {
+					vm.historicalData = data;
+				});
+			}
+		}
+
+		function changePercentTimespan(timespan) {
+			if(timespan !== vm.percentTimespan) {
+				vm.percentTimespan = timespan;
+
+				Hobo.getPercentZNE(vm.percentTimespan, vm.building).then(function(data) {
+					vm.percentData = data;
+				});
+			}
+		}
 
 	}
 
---
+++ web/public/modules/dashboard/services/hobo.client.service.js
@@ -0,0 +1,48 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('dashboard')
+		.factory('Hobo', Hobo);
+
+	Hobo.$inject = ['$http'];
+
+	function Hobo($http) {
+
+		var factory = {
+			getLeaderboard: getLeaderboard,
+			getCurrent: getCurrent,
+			getHistorical: getHistorical,
+			getPercentZNE: getPercentZNE,
+		};
+
+		return factory;
+
+		////////////
+
+		function getLeaderboard() {
+			return $http.get('./api/leaderboard').then(function(res) {
+				return res.data;
+			});
+		}
+
+		function getCurrent(building) {
+			return $http.get('./api/current/'+building).then(function(res) {
+				return res.data;
+			});
+		}
+
+		function getHistorical(timespan, building) {
+			return $http.get('./api/historical/'+timespan+'/'+building).then(function(res) {
+				return res.data;
+			});
+		}
+
+		function getPercentZNE(timespan, building) {
+			return $http.get('./api/percentzne/'+timespan+'/'+building).then(function(res) {
+				return res.data;
+			});
+		}
+	}
+
+})();

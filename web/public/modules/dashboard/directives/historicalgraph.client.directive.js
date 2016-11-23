e0794c8cb4fc9acbdbc95cfaddff252da068f785
Matthew Favetti <mfavetti@gmail.com>
2016-11-22 18:19:17 -0800

    Adding legend to historical graph

--- web/public/modules/dashboard/directives/historicalgraph.client.directive.js
+++ web/public/modules/dashboard/directives/historicalgraph.client.directive.js
@@ -24,7 +24,7 @@
 		function link(scope, element) {
 			d3Service.d3().then(function(d3) {
 
-				var margin = {top: 30, right: 110, bottom: 50, left: 110};
+				var margin = {top: 30, right: 130, bottom: 50, left: 110};
 
 				var svg = d3.select(element[0])
 							.append('svg')
